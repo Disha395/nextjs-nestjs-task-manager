64d20d04e2dddbcc49177bf0fc68e1257888e678
disha <dishanayak395@gmail.com>
2026-05-07 13:57:37 +0530

    add JWT login authentication

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,10 +2,40 @@ import { Injectable } from '@nestjs/common';
 import {UsersService} from "../users/users.service";
 import {SignupDto} from "./dto/signup.dto";
 import * as bcrypt from 'bcrypt';
+import {JwtService} from "@nestjs/jwt";
+import {LoginDto} from "./dto/login.dto";
+import { UnauthorizedException } from '@nestjs/common';
+
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UsersService) {}
+    constructor(private userService: UsersService,
+    private jwtService: JwtService
+) {}
+
+
+async login(dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.email);
+
+    if (!user) {
+        throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+        dto.password,
+        user.password,
+    );
+
+    if (!isPasswordValid) {
+        throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const token = this.jwtService.sign({ userId: user.id });
+
+    return {
+        access_token: token,
+    };
+}
 
 
     async signup(dto : SignupDto){
