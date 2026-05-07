7249afdf242603be8a1947a2e40becaf1793f056
disha <dishanayak395@gmail.com>
2026-05-07 12:39:57 +0530

    implement user signup

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import {UsersService} from "../users/users.service";
+import {SignupDto} from "./dto/signup.dto";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UsersService) {}
+
+
+    async signup(dto : SignupDto){
+        const existingUser = await this.userService.findByEmail(dto.email);
+
+        if(existingUser){
+            throw new Error("User already exists");
+        }
+
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+        const user = await this.userService.create(dto.email, hashedPassword);
+
+        return {
+            message: "User created successfully",
+            user: {
+                id : user.id,
+                email : user.email,
+            }
+        }
+    }
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async findByEmail(email: string) {
+        return this.prismaService.user.findUnique({
+            where: {
+                email,
+            },
+        });
+    }
+
+    async create(email: string, password: string) {
+        return this.prismaService.user.create({
+            data:{
+                email,
+                password,
+            }
+        });
+    }
+
+
+}
